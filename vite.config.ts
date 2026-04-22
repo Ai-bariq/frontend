e1af7a03ae54b83f0ddf11da83f43fafb0176c1f
Rehab_Elkadim <roby.mahmoud.rm@gmail.com>
2026-04-22 15:12:58 +0200

    vercel.json fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
-
-export default config
